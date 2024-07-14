aad1853877dc81f76f151e4784d61cd166a04029
prach14 <47778898+prach14@users.noreply.github.com>
2024-07-14 17:47:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript code for the interactive button
+document.getElementById("clickButton").addEventListener("click", function() {
+    document.getElementById("message").textContent = "Button Clicked!";
+});
